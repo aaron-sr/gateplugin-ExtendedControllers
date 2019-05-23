426814abc2946b91ad208eab42756e55a38acc52
aaron-sr <aaron.schlutter@tu-berlin.de>
2019-05-23 15:18:04 +0200

    remove unused import

--- src/main/java/gate/controllers/ParallelDocumentAnalyserController.java
+++ src/main/java/gate/controllers/ParallelDocumentAnalyserController.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.log4j.Logger;
 
